72d671d375be769afca5f0d449b4ff5b03a838d9
xialuna <xianhuicheng01@gmail.com>
2025-02-07 02:08:11 +0800

    Update tailwind config based on design system

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,13 @@
 import type { Config } from "tailwindcss";
-
 const globalColors = {
-	brand: {
-		primary: "#00878F",
-		secondary: "#E47128",
-		accent: "#21935B",
+	primary: "#008080",
+	secondary: "#EE7402",
+	accent: "#1A6641",
+	white: {
+		50: "#E8E8EA",
+		100: "#FFFFFF",
 	},
-	neutral: {
-		50: "#FFFFFF",
-		100: "#F9FBF7",
-		200: "#FFFFFF",
-		300: "#FFFFFF",
-		400: "#FFFFFF",
-		500: "#000000",
-	},
+	black: "#131118",
 };
 
 export default {
@@ -23,28 +17,30 @@ export default {
 		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
+		colors: globalColors,
 		extend: {
 			fontFamily: {
-				inter: ["Inter", "sans-serif"],
+				montserrat: ["Montserrat", "sans-serif"],
+				morganite: ["Morganite", "sans-serif"],
 			},
-			colors: globalColors,
-			backgroundColor: {
-				"brand-primary": globalColors.brand.primary,
-				"brand-secondary": globalColors.brand.secondary,
-				"brand-primary-hover": globalColors.neutral[500],
-				"neutral-primary": globalColors.neutral[500],
-				"neutral-secondary": globalColors.neutral[100],
-				"neutral-emphasis": globalColors.neutral[200],
+			backgroundImage: {
+				"gradient-hover": `linear-gradient(135deg, ${globalColors.primary} 0%, #42B7B7 44%, #71DEDF 77%, #7DC5C7 100%)`,
+				"gradient-clicked":
+					"linear-gradient(135deg, #42B7B7 4%, #E2ECED 78%, #7DC5C7 100%)",
+				"gradient-border-default":
+					"linear-gradient(0deg, #66F7F7 0%, #B3FBFB 63%, #FFFFFF 100%);",
+				"gradient-border-active": `linear-gradient(0deg, #F8F7FC 0%, ${globalColors.accent} 100%)`,
+				"gradient-border-transparent":
+					"linear-gradient(180deg, #D6EAEA 24%, #1A1A1C 100%)",
 			},
-			borderColor: {
-				"neutral-gray": "#DDDCDC",
-			},
-			textColor: {
-				"brand-onprimary": globalColors.neutral[50],
-				"brand-onsecondary": globalColors.neutral[400],
-				"neutral-primary": globalColors.neutral[400],
-				"neutral-primary-hover": globalColors.neutral[50],
-				"neutral-secondary": globalColors.neutral[300],
+			fontSize: {
+				sm: ["13.3px", { letterSpacing: "0.005em" }],
+				base: ["16px", { letterSpacing: "0.005em" }],
+				xl: ["19.2px", { letterSpacing: "0.005em" }],
+				"2xl": ["36.2px", { letterSpacing: "0.05em" }],
+				"3xl": ["51.99px", { letterSpacing: "0.05em" }],
+				"4xl": ["55.23px", { letterSpacing: "0.05em" }],
+				"5xl": ["73.96px", { letterSpacing: "0.05em" }],
 			},
 		},
 	},
