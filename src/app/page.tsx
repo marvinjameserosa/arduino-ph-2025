4768d063ac7589374e08b48f1ac139ca23dd74ea
xialuna <xianhuicheng01@gmail.com>
2025-02-08 02:23:51 +0800

    Add grid component (no configurations)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
-import { Button } from "@/components/ui/button";
-import { LampContainer } from "@/components/ui/lamp";
-
 export default function Home() {
 	return (
 		<div>
 			<h1>Hello World</h1>
-			<Button className="text-xl px-11 py-7">Register Now</Button>
-			<LampContainer>
-				<h2 className="font-morganite uppercase">Featured Speakers</h2>
-			</LampContainer>
+			<div className="h-screen py-20 w-full"></div>
 		</div>
 	);
 }
