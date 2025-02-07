63399f4c42a8cf4169b2b918ff80b18b527f118a
xialuna <xianhuicheng01@gmail.com>
2025-02-07 22:53:21 +0800

    Add reusable button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/Button";
+
 export default function Home() {
 	return (
 		<div>
 			<h1>Hello World</h1>
+			<Button className="text-xl px-11 py-7">Register Now</Button>
 		</div>
 	);
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,11 +24,10 @@ export default {
 				morganite: ["Morganite", "sans-serif"],
 			},
 			backgroundImage: {
-				"gradient-hover": `linear-gradient(135deg, ${globalColors.primary} 0%, #42B7B7 44%, #71DEDF 77%, #7DC5C7 100%)`,
+				"gradient-hover": `linear-gradient(130deg, ${globalColors.primary} 0%, #42B7B7 44%, #71DEDF 77%, #7DC5C7 100%)`,
 				"gradient-clicked":
 					"linear-gradient(135deg, #42B7B7 4%, #E2ECED 78%, #7DC5C7 100%)",
-				"gradient-border-default":
-					"linear-gradient(0deg, #66F7F7 0%, #B3FBFB 63%, #FFFFFF 100%);",
+				"gradient-border-default": `linear-gradient(0deg, ${globalColors.primary} 0%, #66F7F7 63%, #FFFFFF 100%);`,
 				"gradient-border-active": `linear-gradient(0deg, #F8F7FC 0%, ${globalColors.accent} 100%)`,
 				"gradient-border-transparent":
 					"linear-gradient(180deg, #D6EAEA 24%, #1A1A1C 100%)",
@@ -42,6 +41,21 @@ export default {
 				"3xl": ["51.99px", { letterSpacing: "0.05em" }],
 				"4xl": ["55.23px", { letterSpacing: "0.05em" }],
 				"5xl": ["73.96px", { letterSpacing: "0.05em" }],
+			},
+
+			animation: {
+				shimmer: "shimmer 2s linear infinite",
+			},
+
+			keyframes: {
+				shimmer: {
+					from: {
+						backgroundPosition: "0 0",
+					},
+					to: {
+						backgroundPosition: "-200% 0",
+					},
+				},
 			},
 		},
 	},
