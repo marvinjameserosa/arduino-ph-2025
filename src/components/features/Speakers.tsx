27916c3a121fb2594b8077ce9c372a6625e3e1df
xialuna <xianhuicheng01@gmail.com>
2025-02-11 17:19:26 +0800

    feat(speakers}: create speakers section with automatic hovering

---
+++ src/components/features/Speakers.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React from "react";
+import { LampContainer } from "../ui/lamp";
+import { InfiniteMovingCards } from "../ui/infinite-moving-cards";
+import speakers from "@/data/index";
+
+const Speakers = () => {
+	return (
+		<div className="relative">
+			<LampContainer>
+				<h2>Speakers</h2>
+			</LampContainer>
+			<InfiniteMovingCards
+				items={speakers}
+				direction="right"
+				speed="normal"
+				className="absolute top-2/4 mt-24"
+			/>
+		</div>
+	);
+};
+
+export default Speakers;
