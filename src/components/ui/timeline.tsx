9e0b5e38b2902282b973b6179186fa4849cd9cf6
maiei <146412593+marie-criz@users.noreply.github.com>
2025-02-15 16:33:42 +0800

    chore: removed unnecessary visual components, added flag and divider line

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -1,75 +1,74 @@
 "use client";
-import { useScroll, useTransform, motion } from "framer-motion";
-import React, { useEffect, useRef, useState } from "react";
+import React, {useRef} from "react";
+import {Flag} from 'lucide-react';
 
 interface TimelineEntry {
-	title: string;
-	content: React.ReactNode;
+    title: React.ReactNode;
+    content: React.ReactNode;
 }
 
-export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
-	const ref = useRef<HTMLDivElement>(null);
-	const containerRef = useRef<HTMLDivElement>(null);
-	const [height, setHeight] = useState(0);
+export const Timeline = ({data}: { data: TimelineEntry[] }) => {
+    const ref = useRef<HTMLDivElement>(null);
+    const containerRef = useRef<HTMLDivElement>(null);
 
-	useEffect(() => {
-		if (ref.current) {
-			const rect = ref.current.getBoundingClientRect();
-			setHeight(rect.height);
-		}
-	}, [ref]);
+    return (
+        <div
+            className="w-full bg-white font-sans"
+            ref={containerRef}
+        >
+            <div ref={ref} className="relative max-w-7xl mx-auto pb-20">
+                {data.map((item, index) => (
+                    <div key={index}>
+                        <div
+                            className="flex justify-start pt-0 md:gap-10 h-full">
+                            <div
+                                className="flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-[612px] md:w-full">
+                                <div className="relative flex items-start w-full">
 
-	const { scrollYProgress } = useScroll({
-		target: containerRef,
-		offset: ["start 10%", "end 50%"],
-	});
+                                    <div className="relative">
+                                        {/*circle  with gradient border*/}
+                                        <div
+                                            className={`w-[25px] h-[25px] md:w-[60px] md:h-[60px] absolute mt-1 
+                                                        ${index % 2 === 0 ? 'border-gradient-primary' : 'border-gradient-secondary'}`}/>
 
-	const heightTransform = useTransform(scrollYProgress, [0, 1], [0, height]);
-	const opacityTransform = useTransform(scrollYProgress, [0, 0.1], [0, 1]);
+                                        {/*flag container*/}
+                                        <div
+                                            className="absolute mt-1 w-[25px] h-[25px] md:w-[60px] md:h-[60px] flex items-center justify-center">
+                                            <Flag className="w-[12px] h-[12px] stroke-[3px] md:h-[25px] md:w-[25px] z-10"/>
+                                        </div>
 
-	return (
-		<div
-			className="w-full bg-white bg-black font-sans md:px-10"
-			ref={containerRef}
-		>
-			<div ref={ref} className="relative max-w-7xl mx-auto pb-20">
-				{data.map((item, index) => (
-					<div
-						key={index}
-						className="flex justify-start pt-10 md:pt-40 md:gap-10"
-					>
-						<div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
-							<div className="h-10 absolute left-3 md:left-3 w-10 rounded-full flex items-center justify-center">
-								<div className="h-4 w-4 rounded-full bg-neutral-200 border border-neutral-300 dark:border-neutral-700 p-2" />
-							</div>
-							<h2 className="hidden md:block text-xl md:pl-20 md:text-5xl">
-								{item.title}
-							</h2>
-						</div>
+                                        {/*vertical line connects the flags*/}
+                                        {index < data.length - 1 && (
+                                            <div
+                                                className={`absolute left-[12px] top-[29px] md:left-[29px] md:top-[63px] w-[1px] h-[200px] md:h-[350px] xl:h-[265px]
+                                                            bg-gradient-to-b 
+                                                            ${index === 0 ? 'from-secondary' : 'from-primary'} 
+                                                            via-white-50 to-transparent`}
+                                            />
+                                        )}
+                                    </div>
 
-						<div className="relative pl-20 pr-4 md:pl-4 w-full">
-							<h2 className="md:hidden block text-2xl mb-4 text-left">
-								{item.title}
-							</h2>
-							{item.content}{" "}
-						</div>
-					</div>
-				))}
-				<div
-					style={{
-						height: height + "px",
-					}}
-					className="absolute md:left-8 left-8 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-200 dark:via-neutral-700 to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
-				>
-					<motion.div
-						style={{
-							height: heightTransform,
-							opacity: opacityTransform,
-						}}
-						className="absolute inset-x-0 top-0  w-[2px] bg-gradient-to-t from-secondary via-primary to-transparent from-[0%] via-[10%] rounded-full"
-					/>
-				</div>
-			</div>
-		</div>
-	);
+                                    <h2 className="hidden md:block text-xl md:pl-20 md:text-5xl">
+                                        {item.title}
+                                    </h2>
+                                </div>
+                            </div>
+
+                            <div className="relative pl-10 pr-4 md:pl-4 w-full">
+                                <h2 className="md:hidden block text-2xl mb-4 text-left">
+                                    {item.title}
+                                </h2>
+                                {item.content}
+                            </div>
+                        </div>
+
+                        {/*div line*/}
+                        <div className="relative w-full pt-14 md:pt-16 md:h-[200px] h-[10px]">
+                            <div className="absolute bottom-1/2 right-[5%] w-[85%] lg:w-[90%] h-[1px] bg-gray-200/20 " />
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
