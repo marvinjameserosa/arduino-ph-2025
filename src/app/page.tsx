73e826b5edd02711728807c865adbf61273b22df
maiei <146412593+marie-criz@users.noreply.github.com>
2025-02-16 05:38:09 +0800

    feat(page): added Agenda component to the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
-	return <div></div>;
+import Agenda from "@/components/features/Agenda";
+
+export default function Page() {
+    return (
+            <div className="relative z-10">
+                <Agenda />
+            </div>
+    );
 }
