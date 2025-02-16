41a0b5fe5b4d8273fdc7003f591142a06c26ecfe
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2025-02-17 02:57:33 +0800

    Revert "feat: add Sponsors component to the homepage"
    This reverts commit 9dc108dd5f55bb3e11331515157ba0d7c858c44f.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Footer from "@/components/features/Footer";
 import Location from "@/components/features/Location";
 import Navbar from "@/components/features/Navbar";
 import Speakers from "@/components/features/Speakers";
-import Sponsors from "@/components/features/Sponsors";
 import Merch from "@/components/features/Merch";
 
 export default function Home() {
@@ -11,7 +10,6 @@ export default function Home() {
 			<Navbar />
 			<Location />
 			<Speakers />
-			<Sponsors />
 			<Merch />
 			<Footer />
 		</div>
