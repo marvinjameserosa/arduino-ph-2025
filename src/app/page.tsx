fbd3ad051e017f528bbd9af5af64282515455d80
marvcom <marvinjameserosa@gmail.com>
2025-03-01 16:59:47 +0800

    change the favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Footer from "@/components/features/Footer";
 import CtaBanner from "@/components/features/CtaBanner";
 import Location from "@/components/features/Location";
 import Navbar from "@/components/features/Navbar";
-import Speakers from "@/components/features/Speakers";
+//import Speakers from "@/components/features/Speakers";
 import Merch from "@/components/features/Merch";
 import { Hero } from "@/components/features/Hero";
 import AboutUs from "@/components/features/AboutUs";
