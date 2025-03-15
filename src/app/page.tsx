c3017a6b7de3f8eca04501c80559765d0a2e322e
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2025-03-15 21:46:56 +0800

    feat(partners): add Partners component to display sponsors with responsive logos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,27 +4,29 @@ import Footer from "@/components/features/Footer";
 import CtaBanner from "@/components/features/CtaBanner";
 import Location from "@/components/features/Location";
 import Navbar from "@/components/features/Navbar";
-//import Speakers from "@/components/features/Speakers";
+import Speakers from "@/components/features/Speakers";
 import Merch from "@/components/features/Merch";
 import { Hero } from "@/components/features/Hero";
 import AboutUs from "@/components/features/AboutUs";
+import Partners from "@/components/features/Partners";
 
 export default function Home() {
 	return (
-		<div>
+		<div className="overflow-x-hidden">
 			<Navbar />
 			<Hero />
 			<section id="about">
 				<AboutUs />
 			</section>
 			<Location />
-			{/* 
 			<section id="speakers">
 				<Speakers />
 			</section>
-			*/}
 			<section id="agenda">
 				<Agenda />
+			</section>
+			<section id="partners">
+				<Partners />
 			</section>
 			<section id="merch">
 				<Merch />
